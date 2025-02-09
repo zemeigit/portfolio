eb83163bedaf51058e08a49386e1fe785884fa4f
zemeigit <wzmca94@gmail.com>
2025-02-09 17:48:03 -0500

    initial

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 2;
+
+export const dialogueData = {
+  pc: `My Technical Stack: <br>
+  Programming Language: JavaScript, TypeScript, Java, Python,<br>
+  Frontend: React, Redux, HTML, CSS,<br>
+  Backend: Node.js, Express, Vite,<br>
+  Database: MongoDB, MySQL,<br>
+  DevOps: Docker, Kubernetes, Jenkins, Azure, Helm...`,
+  "cs-degree": `This is my Master's degree in Software Engineering from Concordia University. I hung it on the wall because I'm proud of it. The program was challenging, but I believe it gave me a strong foundation and deepened my understanding of software development.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. If you are interested in my <a href="https://www.youtube.com/@Adadaladala" target="_blank">Youtube</a> channel, which documents some aspects of my life, but I'm lazy and don't update it very often.`,
+  bed: `This where I sleep, and I think it is the most comfortable place in the world.`,
+  resume: `This is my desk and on it is my resume. I am a Full-Stack Software Engineer in Manulife Financial Corporation.`,
+  projects: `Have you discovered more about me? I love rock climbing, traveling and outdoor sports...And, freedom.`,
+  library: `Keep learning and improving, be brave and be yourself, today’s me is happier than yesterday.`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+};
